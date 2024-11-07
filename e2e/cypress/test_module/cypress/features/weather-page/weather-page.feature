44f9bfc6626e4eb61b2f1d320c6c1dfd6aef91be
dmtrth25 <vodolazhchenko25@gmail.com>
2024-11-07 11:08:58 +0200

    changed naming for e2e tests and used httpMethod obj

--- e2e/cypress/test_module/cypress/features/weather-page/weather-page.feature
+++ e2e/cypress/test_module/cypress/features/weather-page/weather-page.feature
@@ -1,24 +1,24 @@
-Feature: | Weather Search |
-    Verify that the weather search feature works correctly
+Feature: | Weather page |
+  Verify that the weather search feature works correctly
 
-    Background: Weather page context
-        Given The user is on the weather page
+  Background: User is on the Weather page
+    Given I am on the weather page
 
-    Scenario: Search for a city's weather
-        When The user enters "Kyiv" into the search input
-        And The user submits the search
-        Then The weather information for "Kyiv" should be displayed
+  Scenario: Search for a city's weather
+    When I enter "Kyiv" into the search input
+    And I submit the search
+    Then I should see the weather information for "Kyiv"
 
-    Scenario: Display error when weather data cannot be retrieved
-        Given The user's connection is bad
-        When The user searches for a city's weather
-        Then An error message should be displayed
+  Scenario: Display error when weather data cannot be retrieved
+    Given My network connection is bad
+    When I search for a city's weather
+    Then I should see an error message
 
-    Scenario: Display message when search input is empty
-        When The user clears the search input
-        Then A prompt message should be displayed asking the user to type a search query
+  Scenario: Display message when search input is empty
+    When I clear the search input
+    Then I should see a prompt message asking me to type a search query
 
-    Scenario: Perform search using Enter key
-        When The user enters "Lviv" into the search input
-        And The user presses Enter
-        Then The weather information for "Lviv" should be displayed
+  Scenario: Perform search using the Enter key
+    When I enter "Lviv" into the search input
+    And I press Enter
+    Then I should see the weather information for "Lviv"
--- e2e/cypress/test_module/cypress/support/step_definitions/weather-page.steps.ts
+++ e2e/cypress/test_module/cypress/support/step_definitions/weather-page.steps.ts
@@ -1,58 +1,56 @@
 /// <reference types="cypress" />
 
 import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+import { httpMethod } from "@cypress-e2e/fixtures/global-data";
 
-Given("The user is on the weather page", () => {
-  cy.intercept("GET", "**/data/2.5/weather*").as("weatherRequest");
+Given("I am on the weather page", () => {
+  cy.intercept(httpMethod.get, "**/data/2.5/weather*").as("weatherRequest");
   cy.visit("/weather");
 });
 
-When("The user enters {string} into the search input", (city: string) => {
+When("I enter {string} into the search input", (city: string) => {
   cy.getById("app-search-input").type(city);
 });
 
-When("The user submits the search", () => {
+When("I submit the search", () => {
   cy.getById("app-search-input-submit").click();
   cy.wait("@weatherRequest");
 });
 
-When("The user presses Enter", () => {
+When("I press Enter", () => {
   cy.getById("app-search-input").type("{enter}");
   cy.wait("@weatherRequest");
 });
 
-Then("The weather information for {string} should be displayed", () => {
+Then("I should see the weather information for {string}", () => {
   cy.get(".spa-weather-card__container").should("be.visible");
 });
 
-Given("The user's connection is bad", () => {
-  cy.intercept("GET", "**/data/2.5/weather*", {
+Given("My network connection is bad", () => {
+  cy.intercept(httpMethod.get, "**/data/2.5/weather*", {
     statusCode: 500,
     body: { message: "Internal Server Error" }
   }).as("weatherRequestError");
   cy.visit("/weather");
 });
 
-When("The user searches for a city's weather", () => {
+When("I search for a city's weather", () => {
   cy.getById("app-search-input").type("Test City{enter}");
   cy.wait("@weatherRequestError");
 });
 
-Then("An error message should be displayed", () => {
+Then("I should see an error message", () => {
   cy.getById("weather-error-message").should("be.visible");
 });
 
-When("The user clears the search input", () => {
-  cy.get('[data-cy="app-search-input"]').type("Test City");
-  cy.get('[data-cy="app-search-input-clear"]').click();
+When("I clear the search input", () => {
+  cy.getById("app-search-input").type("Test City");
+  cy.getById("app-search-input-clear").click();
 });
 
-Then(
-  "A prompt message should be displayed asking the user to type a search query",
-  () => {
-    cy.get(".spa-search-message").should(
-      "contain",
-      "Please type the input search"
-    );
-  }
-);
+Then("I should see a prompt message asking me to type a search query", () => {
+  cy.get(".spa-search-message").should(
+    "contain",
+    "Please type the input search"
+  );
+});
