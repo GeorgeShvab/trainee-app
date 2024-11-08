73c94641bb2bbe9477b5bcb4812a18250c15c6e3
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-11-08 16:02:06 +0200

    removed redundant code

--- code/src/components/app-search-input/AppSearchInput.types.ts
+++ code/src/components/app-search-input/AppSearchInput.types.ts
@@ -1,10 +1,6 @@
 import { InputBaseProps } from "@mui/material/InputBase";
 
-import { AppIconButtonProps } from "@/components/app-icon-button/AppIconButton.types";
-
 export type AppSearchInputProps = InputBaseProps & {
   onSearch?: () => void;
   onClear?: () => void;
-  submitButtonProps?: AppIconButtonProps;
-  clearButtonProps?: AppIconButtonProps;
 };
